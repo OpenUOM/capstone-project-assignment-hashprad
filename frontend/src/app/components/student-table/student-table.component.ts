72c9b2327f9f8e198d1e40e55ee758abee8a6406
hashprad <hashpradeep@gmail.com>
2023-01-13 17:58:49 +0530

    frontend update

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -34,6 +34,14 @@ export class StudentTableComponent implements OnInit {
     this.router.navigate(['editStudent'], navigationExtras )
   }
 
+  initializeDB(){
+    this.service.initializeDB().subscribe((response) => {
+      console.log('DB is Initialized')
+    }, (error) => {
+      console.log('ERROR - ', error)
+    })
+  }
+
   getStudentData(){
     this.selected = 'Students';
     this.service.getStudentData().subscribe((response)=>{
