9107801465549e9c5d4df293e2091c3893285057
hashprad <hashpradeep@gmail.com>
2023-01-13 18:35:50 +0530

    frontend update

--- frontend/src/app/components/add-new-teacher/add-new-teacher.component.ts
+++ frontend/src/app/components/add-new-teacher/add-new-teacher.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AppServiceService} from '../../app-service.service';
+import {AppServiceService} from 'src/app/app-service.service';
 import { Router } from '@angular/router';
 
 @Component({
